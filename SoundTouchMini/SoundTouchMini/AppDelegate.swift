799cb9e8ff0be0167a4de922def96bbcb3f6e36b
robbystoned <robbystoned@air-van-robert.fritz.box>
2017-05-10 21:08:15 +0200

    initial workspace

---
+++ SoundTouchMini/SoundTouchMini/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SoundTouchMini
+//
+//  Created by Robert vd Steen on 10-05-17.
+//  Copyright © 2017 Robert vd Steen. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SoundTouchMini/SoundTouchMini/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SoundTouchMini
+//
+//  Created by Robert vd Steen on 10-05-17.
+//  Copyright © 2017 Robert vd Steen. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
